58a88db453f7a3d856c1546f1700917b2080b74c
flourishingsentries <flourishingsentries@gmail.com>
2022-01-08 13:59:28 -0500

    Added RPC calls to get more data for the server. This includes name and symbol for each contract that adheres to the standards

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-const { writeFile } = require('fs/promises')
-const fastify = require('fastify')({ logger: true })
+const { writeFile } = require('fs/promises');
+const { providers } = require('near-api-js');
+const fastify = require('fastify')({ logger: true });
 
 fastify.register(require('fastify-postgres'), {
 	connectionString: 'postgres://public_readonly:nearprotocol@testnet.db.explorer.indexer.near.dev/testnet_explorer'
-})
+});
 
 const queries = {
 	contracts: () => new Promise((res, rej) => {
+		const provider = new providers.JsonRpcProvider("https://rpc.testnet.near.org");
+
 		fastify.pg.connect(onConnect = (err, client, release) => {
 			if (err) {
-				return rej(err)
+				return rej(err);
 			}
 	
 			client.query(
@@ -26,35 +29,66 @@ const queries = {
 					desc
 				`, [],
 				onResult = async (err, result) => {
-					release()
+					release();
 					if (err) {
-						return rej(err)
+						return rej(err);
 					}
 
-					await writeFile('../static/data.json', JSON.stringify(result.rows))
+					let formattedRows = []; 
 
-					res(result.rows)
+					//loop through each row of the result and gets metadata information from RPC
+					for(var i = 0; i < result.rows.length; i++) {
+						try {
+							//get the symbol and name for the contract. If the provider can't call the nft_metadata function, skips contract.
+							const data = await getContractMetadata(provider, result.rows[i].contractId);
+							data.contractId = result.rows[i].contractId; 
+							data.ts = result.rows[i].ts;
+							formattedRows.push(data); 
+						} catch(e) {
+							console.log("Skipping. Error for contract: ", result.rows[i].contractId);
+						}
+						console.log("Finished ", i+1, " of ", result.rows.length);
+					}
+
+					await writeFile('../static/data.json', JSON.stringify(formattedRows));
+
+					res(formattedRows);
 				}
-			)
+			);
 			
-		})
+		});
 	})
+};
+
+async function getContractMetadata(provider, accountId) {
+	const rawResult = await provider.query({
+	  request_type: "call_function",
+	  account_id: accountId,
+	  method_name: "nft_metadata",
+	  args_base64: "e30=",
+	  finality: "optimistic",
+	});
+  
+	// format result
+	const res = JSON.parse(Buffer.from(rawResult.result).toString());
+	return {name: res.name, symbol: res.symbol};
 }
 
 fastify.get('/', async (request, reply) => {
-	return { hello: 'world' }
-})
+	return { hello: 'world' };
+});
 
 fastify.get('/contracts', (req, reply) => {
-	return queries.contracts()
-})
+	return queries.contracts();
+});
 
 const start = async () => {
 	try {
-		await fastify.listen(3000)
+		console.log("Hello World!");
+		await fastify.listen(3000);
 	} catch (err) {
-		fastify.log.error(err)
-		process.exit(1)
+		fastify.log.error(err);
+		process.exit(1);
 	}
-}
-start()
+};
+start();
