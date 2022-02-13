147e239a03bb2f47b12ca54e991269ab67c29ac6
chalmundev <chalmundev@protonmail.com>
2022-02-13 15:25:12 -0800

    fix syntax error to weird build error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import { AppProvider } from './state/app.js';
 import { BrowserRouter } from 'react-router-dom';
 import mobile from 'is-mobile';
-export const mobile = mobile();
 
 navigator.serviceWorker.register(new URL('service-worker.js', import.meta.url), {type: 'module'});
 
