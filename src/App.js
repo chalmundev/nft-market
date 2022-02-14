2a6dcb00ad1b8d7c83799e3bbdd682293c3a7623
chalmundev <chalmundev@protonmail.com>
2022-02-13 21:01:53 -0800

    remove resize for mobile

--- src/App.js
+++ src/App.js
@@ -38,6 +38,10 @@ const App = ({ mobile }) => {
 		]);
 		update('loading', false);
 
+		if (mobile) return
+
+		/// resets components after debounce onresize (desktop only)
+
 		window.onresize = () => {
 			update('loading', true)
 			if (resizeTimeout) clearTimeout(resizeTimeout)
